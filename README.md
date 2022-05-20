647cacbad1211ceb3f542e968a95f7e7a10951e6
8LWXpg <v3ak6xhthk@gmail.com>
2022-05-20 14:54:33 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# pdfOCER
+
+Make pdf searchable via powershell command line
+
+## Requiremanets
+
+- [ghostscript](https://www.ghostscript.com/)
+- [tesseract](https://github.com/tesseract-ocr/tesseract)
+- [pdftk](https://www.pdflabs.com/tools/)
+
+All installed and added to the PATH
+
+## Usage
+
+`ps> pdfOCR.ps1 <pdf> [<parallel threads>]`
--- pdfOCR.ps1
+++ pdfOCR.ps1
@@ -3,31 +3,39 @@ param (
     [System.IO.FileInfo]$pdf,
     [int]$spilt = 5
 )
+
 if ($pdf.Extension -ne '.pdf') {
-    'no exist file or pdf'
-    return
+    $pdf = Read-Host 'pdf path'
 }
+
+# get pdf pages
 $pages = [Convert]::ToInt32( ((pdftk.exe $pdf dump_data_utf8 | Select-String 'NumberOfPages').ToString() -replace "[^0-9]", ''), 10)
 if (!$?) {
     return
 }
+
 $TempFolder = "$env:temp\pdfOCR"
 mkdir $TempFolder -Force | Out-Null
+
+# change $spilt in case $pages < $spilt
 $spilt = $spilt -gt $pages ? $pages : $spilt
 0..($spilt - 1) | ForEach-Object -Parallel {
     $tiff = "$using:TempFolder\$($_.ToString()).tiff"
+    # spilt pdf
     $a = ([math]::Round($using:pages / $using:spilt * $_) + 1).ToString()
     $b = [math]::Round($using:pages / $using:spilt * ($_ + 1)).ToString()
     "converting to tiff $_"
-    gswin64c.exe -q -dNOPAUSE -sDEVICE=tiffg4 "-dFirstPage=$a" "-dLastPage=$b" "-sOutputFile=$tiff" -r300 $using:pdf -c quit
+    gswin64c -q -dNOPAUSE -sDEVICE=tiffg4 "-dFirstPage=$a" "-dLastPage=$b" "-sOutputFile=$tiff" -r300 $using:pdf -c quit
     "OCRing $_"
-    tesseract.exe $tiff "$using:TempFolder\$($_.ToString())" -l eng -c textonly_pdf=1 pdf | Out-Null
+    tesseract $tiff "$using:TempFolder\$($_.ToString())" -l eng -c textonly_pdf=1 pdf | Out-Null
 }
+
+# merge pdf and add bookmark back
 $pdfs = @()
 for ($i = 0; $i -lt $spilt; $i++) {
     $pdfs += "$TempFolder\$i.pdf"
 }
-pdftk.exe $pdfs cat output "$TempFolder\out1.pdf"
-pdftk.exe "$TempFolder\out1.pdf" multibackground $pdf output "$TempFolder\out2.pdf"
-pdftk.exe $pdf dump_data_utf8 output - | pdftk.exe "$TempFolder\out2.pdf" update_info_utf8 - output "$($pdf.Remove($pdf.Length - 4))_OCR.pdf"
+pdftk $pdfs cat output "$TempFolder\out1.pdf"
+pdftk "$TempFolder\out1.pdf" multibackground $pdf output "$TempFolder\out2.pdf"
+pdftk $pdf dump_data_utf8 output - | pdftk.exe "$TempFolder\out2.pdf" update_info_utf8 - output "$($pdf.Remove($pdf.Length - 4))_OCR.pdf"
 Remove-Item $TempFolder -Force -Recurse
